2c243032013ff49fbe18994284d38dfa972cf06d
Pradeep Loganathan <Pradeep Loganathan>
2016-11-23 22:43:50 +0530

    Initial Commit

---
+++ Firstone/src/CalcService/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcService.Service
+{
+    public class Calc
+    {
+        public int add(int i, int j)
+        {    
+            return i + j;
+        }
+    }
+}
---
+++ Firstone/test/CalcService.Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using CalcService.Service;
+
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void AddTest() 
+        {
+            Calc c = new Calc();             
+            Assert.Equal(30, c.add(10,10) );
+        }
+    }
+}
