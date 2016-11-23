770b3a1eb7ff54c888fc126441c926e3d9301d4e
Pradeep Loganathan <Pradeep Loganathan>
2016-11-23 23:25:29 +0530

    Second Commit

---
+++ Lists/src/LinkedList/SLinkedList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lists
+{
+    public class SLinkedList
+    {
+        public int add(int i, int j)
+        {    
+            return i + j;
+        }
+    }
+}
---
+++ Lists/test/LinkedList.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Lists;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            SLinkedList s = new SLinkedList();
+            Assert.Equal(10, s.add(3,2));
+        }
+    }
+}
