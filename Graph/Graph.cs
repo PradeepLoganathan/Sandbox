e6a0864122dc2dc207fb2a24762dcd8740bf48ce
Pradeep Loganathan <Pradeep Loganathan>
2016-11-26 19:30:42 +0530

    Initial graph checkin

---
+++ Graph/Graph.cs
@@ -0,0 +1,15 @@
+using system;
+using System.Collections;
+
+namespace DataStructures.Graphs
+{
+    public class Neighbour
+    {
+
+    }
+
+    class Vertex
+    {
+       
+    }
+}
---
+++ Graph/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures.Graphs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
